a2d95152cd19e6dc3de148c3f7ea41124777b01c
Your Name <9leon1@gmail.com>
2017-10-21 22:09:32 +0300

    add test

---
+++ test.php
@@ -0,0 +1,22 @@
+<table class="table table-bordered">
+    <caption>Таблица умножения</caption>
+    <tr>
+        <th class="text-right">&nbsp;</th>
+        <th class="text-right">2</th>
+        <th class="text-right">3</th>
+        <th class="text-right">4</th>
+        <th class="text-right">5</th>
+        <th class="text-right">6</th>
+        <th class="text-right">7</th>
+        <th class="text-right">8</th>
+        <th class="text-right">9</th>
+    </tr>
+    <?php for ($i = 2; $i <= 9; $i++) : ?>
+    <tr>
+        <?= "<th class=\"text-right\">{$i}</th>" ?>
+        <?php for ($j = 2; $j <= 9; $j++) : ?>
+            <?= '<td class="text-right">' . $i * $j . '</td>' ?>
+        <?php endfor; ?>
+    </tr>
+<?php endfor; ?>
+</table>
